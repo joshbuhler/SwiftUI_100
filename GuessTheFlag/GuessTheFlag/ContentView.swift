62abc6de91466c928fa0af2b259714d904d3273f
Joshua Buhler <josh@buhler.me>
2023-01-14 09:04:04 -0700

    Completes Day 22

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,41 +8,108 @@
 import SwiftUI
 
 struct ContentView: View {
-    func doSomething() {
-        print ("🦖")
-        showingAlert = true
-    }
     
-    @State private var showingAlert = false
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var flagSelected = 0
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var currentScore = 0
+    @State private var currentRound = 1
+    private var maxRounds = 8
+    
+    @State private var gameOver = false
     
     var body: some View {
         ZStack {
-            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+            .ignoresSafeArea()
             
-            VStack{
-                Text ("Content here")
-                    .foregroundStyle(.secondary, .ultraThickMaterial)
-                    .padding(50)
-                    .background(.ultraThinMaterial)
-                Button ("Do Something",
-                        action: doSomething)
-                .alert("Message", isPresented: $showingAlert) {
-                    Button("Make me cry", role: .destructive) { }
-                    Button("Cancel", role: .cancel) { }
-                } message: {
-                    Text("Do you really want to hurt me?")
+            //            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center).ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                Spacer()
+                Spacer()
+                Text("Score: \(currentScore)/\(maxRounds)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                Text("Current Round: \(currentRound)")
+                    .foregroundColor(.white)
+                    .font(.subheadline.smallCaps())
+                Spacer()
+                VStack(spacing: 15) {
+                    VStack{
+                        Text("Tap the flag of…")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
+                    }
                 }
-                .buttonStyle(.borderedProminent)
-                .tint(.mint)
-                Button {
-                    print ("b2")
-                } label: {
-                    Label("Edit", systemImage: "pencil")
-                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
             }
+            .padding()
         }
-        .ignoresSafeArea()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("You picked: \(countries[flagSelected])\nYour score is: \(currentScore)")
+        }
+        .alert("Game Over", isPresented: $gameOver) {
+            Button("New Game", action: newGame)
+        } message: {
+            Text("Your score is: \(currentScore)")
+        }
+    }
+    
+    func flagTapped(_ number:Int) {
+        flagSelected = number
+        if (number == correctAnswer) {
+            scoreTitle = "Correct"
+            currentScore += 1
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
         
+        if (currentRound >= maxRounds) {
+            gameOver = true
+        }
+        currentRound += 1
+    }
+    
+    func askQuestion () {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func newGame () {
+        currentScore = 0
+        currentRound = 1
+        askQuestion()
     }
 }
 
