3e11ea53d99be0f927a5072bc2fa4d9b9ba3f8b0
Joshua Buhler <josh@buhler.me>
2023-01-19 15:12:51 -0700

    Rock Paper Scissors - Not pretty, but it meets the requirements

--- Day25_rps/Day25_rps/ContentView.swift
+++ Day25_rps/Day25_rps/ContentView.swift
@@ -9,38 +9,99 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var currentRound:Int = 0
-    var maxRounds:Int = 10
+    @State var currentRound:Int = 0
+    @State var maxRounds:Int = 10
     
     @State var totalPoints:Int = 0
     
-    @State var shouldWin:Bool = true
-    @State var cpuChoice:Int = 0
+    @State var shouldWin:Bool = Bool.random()
+    @State var cpuChoice:Int = Int.random(in: 0..<2)
+    
+    @State var displayGameOver = false
     
     var options = ["Rock", "Paper", "Scissors"]
+    
+    func checkResult (selected:Int) {
+        var correctAnswer = false
+        
+        if (shouldWin) {
+            if (selected == (cpuChoice + 1) || selected == (cpuChoice - 2)) {
+                correctAnswer = true
+            }
+        } else {
+            if (selected == (cpuChoice - 1) || selected == (cpuChoice + 2)) {
+                correctAnswer = true
+            }
+        }
+        
+        if (correctAnswer) {
+            totalPoints += 1
+        }
+        
+        shouldWin.toggle()
+        cpuChoice = Int.random(in: 0..<2)
+        
+        currentRound += 1
+        if (currentRound >= maxRounds) {
+            displayGameOver = true
+        }
+    }
     
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack{
+                Text("Score: \(totalPoints)")
+                Text("Round: \(currentRound)")
+            }
+            Spacer()
+            
+            Text("\(options[cpuChoice].uppercased())… I choose you!")
+                .font(.largeTitle)
+            Spacer()
+            
+            Text("You should…")
+            Spacer()
+            Text(shouldWin ? "WIN" : "LOSE")
+                .font(.largeTitle)
+            
+            Spacer()
             
             HStack {
+                Spacer()
                 Button("🪨") {
-                    print ("ROCK")
+                    checkResult(selected: RPS.rock.rawValue)
                 }
+                Spacer()
                 Button("📄") {
-                    print ("PAPER")
+                    checkResult(selected: RPS.paper.rawValue)
                 }
+                Spacer()
                 Button("✂️") {
-                    print ("SCISSORS")
+                    checkResult(selected: RPS.scissors.rawValue)
                 }
+                Spacer()
             }
+            .padding()
+            
+            Spacer()
         }
         .padding()
+        .alert("Game Over", isPresented: $displayGameOver) {
+            Button("OK") {
+                currentRound = 0
+                totalPoints = 0
+            }
+        } message: {
+            Text("Final Score: \(totalPoints)")
+        }
     }
+}
+
+enum RPS:Int {
+    case rock
+    case paper
+    case scissors
 }
 
 struct ContentView_Previews: PreviewProvider {
