52bd361f8e95116555a4a940c13fc27db35fa4b3
Joshua Buhler <josh@buhler.me>
2023-01-19 16:31:39 -0700

    Trying out Apple's stuff

---
+++ Apple/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Josh Buhler on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
