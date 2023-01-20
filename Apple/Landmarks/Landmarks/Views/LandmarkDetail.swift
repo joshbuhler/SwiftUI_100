1aac6f60eb24e345f006c6414c7a1b3bd5837962
Joshua Buhler <josh@buhler.me>
2023-01-20 13:18:54 -0700

    Section 7

---
+++ Apple/Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,54 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Josh Buhler on 1/20/23.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    
+    var landmark: Landmark
+    
+    var body: some View {
+        
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack (alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                HStack{
+                    Text(landmark.park)
+                    Spacer()
+                    Text (landmark.state)
+                }
+                .font(.subheadline)
+            }
+            .padding()
+            
+            Divider()
+            
+            Text("About \(landmark.name)")
+                .font(.title2)
+            Text(landmark.description)
+                .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[0])
+    }
+}
