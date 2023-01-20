e3dd31d4f732fddfb60be338cf2c9ffdcdd48d95
Joshua Buhler <josh@buhler.me>
2023-01-20 13:04:25 -0700

    Completes section 5

---
+++ Apple/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Josh Buhler on 1/20/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
--- Apple/Landmarks/Landmarks/Views/LandmarkRow.swift
+++ Apple/Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -26,6 +26,10 @@ struct LandmarkRow: View {
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
         // `landmarks` is a global defined in ModelData.swift
-        LandmarkRow(landmark: landmarks[0])
+        Group{
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
     }
 }
