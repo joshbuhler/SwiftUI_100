4642481fc52c172532ca9168effa164e3ba47fcc
Joshua Buhler <josh@buhler.me>
2023-01-23 15:18:58 -0700

    Experimenting w/ native markdown support

---
+++ Markdown/Markdown/MarkdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownApp.swift
+//  Markdown
+//
+//  Created by Josh Buhler on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
