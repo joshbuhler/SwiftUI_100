a1bd9cab11a08dec8fc3a7d98f6c501d338413cd
Joshua Buhler <josh@buhler.me>
2023-02-28 17:00:00 -0700

    kinda - wish it scaled based on height better though. too easy for the upgrade cards to get taller than the unit

---
+++ GeoReader/GeoReader/ContentView.swift
@@ -0,0 +1,107 @@
+//
+//  ContentView.swift
+//  GeoReader
+//
+//  Created by Josh Buhler on 2/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        //        GeometryReader { geeometry in
+        //            HStack(spacing: 0) {
+        //                Text ("Left")
+        //                    .font(.largeTitle)
+        //                    .foregroundColor(.black)
+        //                    .aspectRatio(88/61, contentMode: .fill)
+        //                    .frame(width: geeometry.size.width * 0.33, height: geeometry.size.height)
+        //                    .background(.yellow)
+        //                Text ("Right")
+        //                    .font(.largeTitle)
+        //                    .foregroundColor(.black)
+        //                    .frame(width: geeometry.size.width * 0.67)
+        //                    .background(.orange)
+        //            }
+        //        }
+        CardGroup()
+    }
+}
+
+struct CardGroup:View {
+    var body: some View {
+        //        GeometryReader { geo in
+        //        HStack () {
+        //                Color.orange
+        //                .frame(idealHeight: 100)
+        ////                    .aspectRatio(88/61, contentMode: .fit)
+        ////                                .frame(maxHeight: .infinity)
+        //                Spacer()
+        //                Color.blue
+        ////                    .aspectRatio(61/88, contentMode: .fit)
+        ////                    .frame(maxHeight: geo.size.height)
+        //            }
+        //            .background(.gray)
+        //        }
+        ////        .frame(height: 200)
+        ////        .fixedSize(horizontal: false, vertical: true)
+        //        .background(.green)
+        
+        //    }
+//        VStack {
+//            UnitDeployment()
+            UnitDeployment()
+            .frame(height: 100)
+//        }
+    }
+}
+
+struct UnitDeployment:View {
+    var body: some View {
+        VStack{
+            HStack{
+                UnitCard()
+                    .fixedSize(horizontal: false, vertical: true)
+                UpgradeCard()
+                    .fixedSize(horizontal: false, vertical: true)
+            }
+            .border(Color.gray)
+            HStack{
+                UpgradeCard()
+                    .fixedSize()
+                UpgradeCard()
+                    .fixedSize()
+                UpgradeCard()
+                    .fixedSize()
+            }
+        }
+        .border(Color.green)
+    }
+}
+
+struct UnitCard:View {
+    var body: some View {
+        Color.orange
+            .opacity(0.5)
+            .aspectRatio(88/61, contentMode: .fit)
+            .frame(idealHeight: 200)
+            .background(.gray)
+    }
+}
+
+struct UpgradeCard:View {
+    var body: some View {
+        Color.blue
+            .opacity(0.25)
+            .aspectRatio(61/88, contentMode: .fit)
+            .frame(idealHeight: 200)
+            .background(.gray)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeoReader/GeoReader/GeoReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoReaderApp.swift
+//  GeoReader
+//
+//  Created by Josh Buhler on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct GeoReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
