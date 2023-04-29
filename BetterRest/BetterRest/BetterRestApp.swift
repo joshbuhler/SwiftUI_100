100378cabbe704fa9993b8ec77409d846e6b9285
Joshua Buhler <josh@buhler.me>
2023-04-29 06:43:48 -0600

    Starting BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Joshua Buhler on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Joshua Buhler on 4/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            
+            Stepper("\(sleepAmount.formatted()) hours",
+                    value: $sleepAmount,
+                    in: 4...12,
+                    step: 0.25)
+            
+            DatePicker("Please enter a date",
+                       selection: $wakeUp,
+                       in: Date.now...,
+                       displayedComponents: .hourAndMinute)
+            .labelsHidden()
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
