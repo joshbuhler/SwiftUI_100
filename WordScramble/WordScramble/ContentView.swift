0864757183d713a87a81a7aebe124c2d7f792371
Joshua Buhler <josh@buhler.me>
2023-05-06 13:31:15 -0600

    Finished word scramble

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,34 +8,108 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
     
     @State private var spelledRight = true
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
-        //            List(people, id: \.self) {
-        //                Text($0)
-        //            }
-        
-        // ...is basically the same as...
-        List {
-            Text("Static Row")
-            
-            ForEach(people, id: \.self) {
-                Text($0)
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                        .onSubmit(addNewWord)
+                }
+                
+                Section {
+                    ForEach (usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
             }
-            
-            Text("Static Row")
-            
-            Button("Check") {
-                spelledRight = spellCheck(text: "word")
-            }
-            
-            HStack {
-                Text("Spelled right:")
-                Text((spelledRight ? "yes" : "no"))
+            .navigationTitle(rootWord)
+        }
+        .onAppear(perform: startGame)
+        .alert(errorTitle,
+               isPresented: $showingError) {
+            Button("OK", role: .cancel) { }
+        } message: {
+            Text(errorMessage)
+        }
+    }
+    
+    func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                // If we are here everything has worked, so we can exit
+                return
             }
         }
+        
+        // If were are *here* then there was a problem – trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func addNewWord () {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        print ("alsdkjf")
+        guard !answer.isEmpty else {
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        guard spellCheck(text: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    
+    func isOriginal (word:String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible (word:String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
     }
     
     func spellCheck (text:String) -> Bool {
@@ -45,6 +119,12 @@ struct ContentView: View {
         let bad = checker.rangeOfMisspelledWord(in: text, range: range, startingAt: 0, wrap: false, language: "en")
         
         return bad.location == NSNotFound
+    }
+    
+    func wordError(title:String, message:String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
     
 }
