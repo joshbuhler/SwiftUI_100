e404c41c44bfb8d33f5edfb90140c4885abef0b9
Joshua Buhler <josh@buhler.me>
2023-05-07 20:15:21 -0600

    Custom animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -12,16 +12,28 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tappity") {
-            animationAmount *= 1.25
+        Button("Tap Me") {
+            // animationAmount += 1
         }
         .padding(50)
         .background(.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.easeOut, value: animationAmount)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
+
     }
 }
 
