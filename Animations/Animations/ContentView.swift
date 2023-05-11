e162a03008187d6fbe7380ff98dcc0248ab0df28
Joshua Buhler <josh@buhler.me>
2023-05-11 08:59:46 -0600

    Controlling animation stack

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -10,29 +10,53 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var animationAmount = 1.0
+    @State private var enabled = false
     
     var body: some View {
-        Button("Tap Me") {
-            // animationAmount += 1
+        
+        print ("animationAmount: \(animationAmount)")
+        
+        return VStack {
+            Stepper("Scale amount",
+                     value: $animationAmount.animation(
+                        .easeInOut(duration: 1).repeatCount(3, autoreverses: true)),
+                     in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                    animationAmount += 360
+                }
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 0, z: 1))
+//            .scaleEffect(animationAmount)
+            
+            Button("Tap Me") {
+                // do nothing
+                enabled.toggle()
+            }
+            .frame(width: 200, height: 200)
+            .background(enabled ? .blue : .red)
+            .animation(nil, value: enabled)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+            .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+            
+            Button("Tap Me") {
+                // do nothing
+            }
+            .frame(width: 200, height: 200)
+            .background(.blue)
+            .foregroundColor(.white)
+            
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
-        }
+        
+        
 
     }
 }
