eebb5ec983287e44bf7cddf38ac3f6e6bb002d77
Joshua Buhler <josh@buhler.me>
2023-05-29 19:38:47 -0600

    Astronaut views

---
+++ Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,39 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Joshua Buhler on 5/29/23.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    
+    let astronaut:Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    
+    static let astronauts:[String:Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["aldrin"]!)
+            .preferredColorScheme(.dark)
+    }
+}
