7333a78d85b5aab78302c13b949ef48f894f1cd4
Joshua Buhler <josh@buhler.me>
2023-06-02 16:32:10 -0600

    Animatable Data

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -9,44 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var amount = 0.0
-
-        var body: some View {
-            VStack {
-                ZStack {
-                    Circle()
-                        .fill(.red)
-                        .frame(width: 200 * amount)
-                        .offset(x: -50, y: -80)
-                        .blendMode(.screen)
-
-                    Circle()
-                        .fill(.green)
-                        .frame(width: 200 * amount)
-                        .offset(x: 50, y: -80)
-                        .blendMode(.screen)
-
-                    Circle()
-                        .fill(.blue)
-                        .frame(width: 200 * amount)
-                        .blendMode(.screen)
+    @State private var insetAmount = 50.0
+    
+    var body: some View {
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    insetAmount = Double.random(in: 10...90)
                 }
-                .frame(width: 300, height: 300)
-
-                Slider(value: $amount)
-                    .padding()
-                
-                Image("CaptainGoose")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                    .saturation(amount)
-                    .blur(radius: (1 - amount) * 20)
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.black)
-            .ignoresSafeArea()
-        }
+    }
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: Double
+    
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
 }
 
 struct ColorCyclingCircle: View {
