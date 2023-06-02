5913401d808e1426d34e28549b163899560a3a57
Joshua Buhler <josh@buhler.me>
2023-06-02 16:35:19 -0600

    AnimatablePair

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -9,16 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var insetAmount = 50.0
+    @State private var rows = 4
+    @State private var columns = 4
     
     var body: some View {
-        Trapezoid(insetAmount: insetAmount)
-            .frame(width: 200, height: 100)
+        Checkerboard(rows: rows, columns: columns)
             .onTapGesture {
-                withAnimation {
-                    insetAmount = Double.random(in: 10...90)
+                withAnimation(.linear(duration: 3)) {
+                    rows = 8
+                    columns = 16
                 }
             }
+    }
+}
+
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+           AnimatablePair(Double(rows), Double(columns))
+        }
+
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+        
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+        
+        return path
     }
 }
 
@@ -29,30 +70,30 @@ struct Trapezoid: Shape {
         get { insetAmount }
         set { insetAmount = newValue }
     }
-
+    
     func path(in rect: CGRect) -> Path {
         var path = Path()
-
+        
         path.move(to: CGPoint(x: 0, y: rect.maxY))
         path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
         path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
         path.addLine(to: CGPoint(x: 0, y: rect.maxY))
-
+        
         return path
-   }
+    }
 }
 
 struct ColorCyclingCircle: View {
     var amount = 0.0
     var steps = 100
-
+    
     var body: some View {
         ZStack {
             ForEach(0..<steps) { value in
                 Circle()
                     .inset(by: Double(value))
-//                    .strokeBorder(color(for: value, brightness: 1), lineWidth: 2)
+                //                    .strokeBorder(color(for: value, brightness: 1), lineWidth: 2)
                     .strokeBorder(
                         LinearGradient(
                             gradient: Gradient(colors: [
@@ -68,14 +109,14 @@ struct ColorCyclingCircle: View {
         }
         .drawingGroup()
     }
-
+    
     func color(for value: Int, brightness: Double) -> Color {
         var targetHue = Double(value) / Double(steps) + amount
-
+        
         if targetHue > 1 {
             targetHue -= 1
         }
-
+        
         return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
