efecb8a90aa486e77e3eb380eb9489b50b739a84
Joshua Buhler <josh@buhler.me>
2023-06-02 14:49:16 -0600

    MissionGrid

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -22,49 +22,67 @@ struct ContentView: View {
     let astronauts:[String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions:[Mission] = Bundle.main.decode("missions.json")
     
-    let columns = [ GridItem(.adaptive(minimum: 150)) ]
+    @State var showGrid = true
     
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission,
-                                        astronauts: astronauts)
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
-                                
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.caption)
-                                        .foregroundColor(.white.opacity(0.5))
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
-                            }
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            }
-                        }
-                    }
-                }
+                MissionGrid(astronauts: astronauts,
+                            missions: missions)
                 .padding([.horizontal, .vertical])
             }
             .navigationTitle("Moonshot")
+            .toolbar(content: {
+                Button(showGrid ? "List" : "Grid") {
+                    showGrid.toggle()
+                }
+            })
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+        }
+    }
+}
+
+struct MissionGrid:View {
+    
+    let astronauts:[String:Astronaut]
+    let missions:[Mission]
+    
+    let columns = [ GridItem(.adaptive(minimum: 150)) ]
+    
+    var body: some View {
+        LazyVGrid(columns: columns) {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission,
+                                astronauts: astronauts)
+                } label: {
+                    VStack {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .padding()
+                        
+                        VStack {
+                            Text(mission.displayName)
+                                .font(.headline)
+                                .foregroundColor(.white)
+                            Text(mission.formattedLaunchDate)
+                                .font(.caption)
+                                .foregroundColor(.white.opacity(0.5))
+                        }
+                        .padding(.vertical)
+                        .frame(maxWidth: .infinity)
+                        .background(.lightBackground)
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.lightBackground)
+                    }
+                }
+            }
         }
     }
 }
