bf72646cdcaa8daaf063bc8c861e4928187bd93a
Joshua Buhler <josh@buhler.me>
2023-06-06 08:50:04 -0600

    CupcakeCorner - starting and loading data from iTunes

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Joshua Buhler on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
