e6212c4e7f816b75e612c1e98c664a32200c376c
Joshua Buhler <josh@buhler.me>
2023-06-06 08:55:05 -0600

    Fun w/ image loading

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -14,6 +14,20 @@ struct ContentView: View {
     var body: some View {
         List(results, id: \.trackId) { item in
             VStack(alignment: .leading) {
+                AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+//                AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } else if phase.error != nil {
+                        Text("There was an error loading the image.")
+                    } else {
+                        ProgressView()
+                    }
+                }
+                .frame(width: 200, height: 200)
+                
                 Text(item.trackName)
                     .font(.headline)
                 Text(item.collectionName)
